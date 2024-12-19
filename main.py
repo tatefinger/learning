c8818035afec50675e55cfed71b431a6f4470358
tatefinger <tatefinger@gmail.com>
2024-12-19 12:49:40 -0500

    INCREDIBLE

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ Lens Design Efficiency Calculator
 Tate Finger, Quang Huynh
 """
 
+import numpy as np
+
 # diameter of lens
 # radius 1
 # radius 2
@@ -12,3 +14,6 @@ Tate Finger, Quang Huynh
 # d = diameter
 # f = focal_length
 # h=f*tan(f/2d)
+
+def calculate_hfov(diameter_size, focal_length):
+    return np.degrees(focal_length / (2 * diameter_size))
