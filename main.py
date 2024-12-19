0307942d2408066bf87ea0d770e6c60f80206a8c
tatefinger <tatefinger@gmail.com>
2024-12-19 12:10:28 -0500

    Update 1

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ Lens Design Efficiency Calculator
 Tate Finger, Quang Huynh
 """
 
-d = diameter
-f = focal_length
-h=f*tan(f/2d)
-
+# diameter of lens
+# radius 1
+# radius 2
+# material
+# thickness
+#
+# d = diameter
+# f = focal_length
+# h=f*tan(f/2d)
