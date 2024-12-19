32972483bb68c2312b57a658badad5e4672c7777
Quang <qth9368@rit.edu>
2024-12-19 14:08:41 -0500

    docstring functions

--- main.py
+++ main.py
@@ -16,15 +16,47 @@ import numpy as np
 # NUMERICAL APERTURE
 
 def calculate_power_1(index, rad1):
+    """
+    calculates power from first surface of the lens
+
+    @param index: Refractive index of lens material
+    @param rad1: Radius of curvative of the first surface (in mm)
+    @return: Power of first surface (in 1\mm)
+    """
     return (index-1)/rad1
 
 def calculate_power_2(index, rad2):
+    """
+    calculates power from second surface of the lens
+
+    @param index: Refractive index of lens material
+    @param rad1: Radius of curvative of the second surface (in mm)
+    @return: Power of second surface (in 1\mm)
+    """
     return (1-index)/rad2
 
 def calculate_total_power(thickness, index, power1, power2):
+    """
+    calculates total power of the lens
+
+    @param thickness: Thickness of lens (in mm)
+    @param index: Refractive index of the lens material at an arbitrary wavelength
+    @param power1: Power of first surface (in 1/mm)
+    @param power2: Power of second surface (in 1/mm)
+
+    @return total power of lens (in 1/mm)
+    """
     return power1 + power2 - ((thickness/index)*power1*power2)
 
 def calculate_numerical_aperture(diameter, efl):
+    """
+    calculates numerial aperature (NA) of lens
+
+    @param diameter: Diameter of lens (in mm)
+    @param ef1: Effective focal length (in mm)
+    
+    @return Numerical aperture (dimensionless)
+    """
     return diameter/(2*efl)
 
 def main():
